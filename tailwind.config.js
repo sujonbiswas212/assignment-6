24001653fdd6458087698a1346930459b49958e5
sujonbiswas212 <wwwsujonbiswas212@gmail.com>
2025-05-13 23:56:08 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+ /** @type {import('tailwindcss').Config} */
+export default {
+   content: ["./src/**/*.{html,js}"],
+   theme: {
+     extend: {
+      colors : {
+        'red1':'#ec003f',
+        'blue1':'#4f39f6',
+        'pink1':'#c800de'
+      }
+     },
+   },
+   plugins: [],
+ }
